0e265f4490bf477d935c2d8ee8499e1570cf1290
AloisioFernandes <68351209+AloisioFernandes@users.noreply.github.com>
2020-09-09 10:40:22 -0300

    página de detalhes

--- project2/src/pages/Detail/index.js
+++ project2/src/pages/Detail/index.js
@@ -1,21 +1,53 @@
-import React from 'react'
-import { View, StyleSheet, Text, Image } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import { View, StyleSheet, Text, Image, TouchableOpacity } from 'react-native'
 import { useNavigation, useRoute } from '@react-navigation/native'
 import { ScrollView } from 'react-native-gesture-handler'
+import { Feather } from '@expo/vector-icons'
+
+import { movie } from '../../mockData'
 
 const Detail = () => {
+  const [info, setInfo] = useState({})
   
   const navigation = useNavigation()
   const route = useRoute()
-
   const routeParams = route.params
+
+  useEffect(() => { //normalmente essas informações seriam parâmetros de rota
+    setInfo(movie)
+  })
+
+  function goBack() {
+    navigation.goBack()
+  }
 
   return (
     <View style={styles.container}>
+      <TouchableOpacity style={styles.goBack} onPress={goBack}>
+        <Feather name="arrow-left" size={35} color='#888' />
+      </TouchableOpacity>
+
       <Text style={styles.titulo}>{routeParams.title}</Text>
       <ScrollView>
       <View style={styles.info}>
         <Image style={styles.poster} source={{ uri: routeParams.poster }} />
+      </View>
+      <Text style={styles.detail}>
+        Ano: {routeParams.year}{'\n'}
+        Duração: {movie.Runtime}{'\n'}
+        Diretor: {movie.Director}{'\n'}
+        Atores: {movie.Actors}{'\n'}
+        Sinopse: {movie.Plot}
+      </Text>
+      <View style={styles.buttonGroup}>
+        <TouchableOpacity style={styles.button}>
+          <Feather name="play" size={26} color='crimson' />
+          <Text style={styles.watch}>Assistir</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button}>
+          <Feather name="list" size={26} color='#777' />
+          <Text style={styles.list}>Adicionar à lista</Text>
+        </TouchableOpacity>
       </View>
       </ScrollView>
     </View>
@@ -28,9 +60,12 @@ const styles = StyleSheet.create({
     backgroundColor: '#000'
   },
 
+  goBack: {
+    marginTop: 30
+  },
+
   titulo: {
     color: '#fff',
-    marginTop: 40,
     fontSize: 30
   },
 
@@ -43,10 +78,40 @@ const styles = StyleSheet.create({
     width: 288,
     height: 432,
     marginTop: 23,
+    marginBottom: 10,
     borderWidth: 15,
     borderColor: '#ddd',
     borderRadius: 8
   },
+
+  detail: {
+    color: '#ddd',
+    fontSize: 18,
+    padding: 10
+  },
+
+  buttonGroup: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginHorizontal: 20
+  },
+
+  button: {
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+
+  watch: {
+    color: 'crimson',
+    fontSize: 18,
+    padding: 10
+  },
+
+  list: {
+    color: '#777',
+    fontSize: 18,
+    padding: 10
+  }
 })
 
 export default Detail
